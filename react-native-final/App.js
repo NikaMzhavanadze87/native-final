c0bd61950f05b98eb97c8d5855ada8d0b07053a1
NikaMzhavanadze87 <n.m.mzhavanadze@gmail.com>
2023-06-22 19:01:07 +0400

    native-final

--- react-native-final/App.js
+++ react-native-final/App.js
@@ -1,15 +1,34 @@
-import React, { useState, useEffect} from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet, Image, ScrollView } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from 'react-native';
+import {
+  NavigationContainer,
+  useNavigation,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useSelector, useDispatch, Provider } from 'react-redux';
+import {
+  configureStore,
+  createSlice,
+} from '@reduxjs/toolkit';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
+    paddingBottom: 20,
   },
   input: {
     width: 250,
@@ -35,19 +54,20 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   productContainer: {
-    backgroundColor:'white',
+    backgroundColor: 'white',
     marginBottom: 10,
-    padding:20,
+    padding: 20,
+    
   },
   imageContainer: {
     width: '100%',
-    aspectRatio: 1, 
+    aspectRatio: 1,
     marginBottom: 10,
   },
   productImage: {
     flex: 1,
     width: '100%',
-    resizeMode: 'cover', 
+    resizeMode: 'cover',
   },
   productName: {
     fontSize: 16,
@@ -70,9 +90,104 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  cartItemContainer: {
+    backgroundColor: 'white',
+    marginBottom: 10,
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width:300,
+  },
+  cartItemImage: {
+    width: 80,
+    height: 80,
+    marginRight: 20,
+  },
+  cartItemInfo: {
+    flex: 1,
+  },
+  cartItemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  cartItemPrice: {
+    fontSize: 14,
+    marginBottom:10,
+  },
+  removeButton: {
+    backgroundColor: 'black',
+    borderRadius: 8,
+    padding: 10,
+  },
+  removeButtonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  totalPriceText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+    
+  },
 });
 
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      state.push(action.payload);
+      AsyncStorage.setItem('cart', JSON.stringify(state));
+    },
+    removeFromCart: (state, action) => {
+      const newState = state.filter((product) => product.id !== action.payload);
+      AsyncStorage.setItem('cart', JSON.stringify(newState));
+      return newState;
+    },
+    initializeCart: (state, action) => {
+      return action.payload || [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice.reducer,
+  },
+});
+AsyncStorage.getItem('cart').then((data) => {
+  store.dispatch(cartSlice.actions.initializeCart(JSON.parse(data)));
+});
+//////////////////////////////////////////
 function HomeScreen() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const isInCart = (productId) => {
+    return cart.some((product) => product.id === productId);
+  };
+
+  const handleAddToCart = (product) => {
+    if (isInCart(product.id)) {
+      dispatch(removeFromCart(product.id));
+    } else {
+      dispatch(addToCart(product));
+    }
+  };
+
+  const calculateTotalPrice = () => {
+    return cart.reduce((total, product) => total + parseFloat(product.original_price), 0);
+  };
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -91,13 +206,6 @@ function HomeScreen() {
     fetchData();
   }, []);
 
-  const handleAddToCart = (productId) => {
-    // Implement your logic for adding the product to the cart
-    console.log('Product added to cart:', productId);
-  };
-
-  
-
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -110,10 +218,12 @@ function HomeScreen() {
               />
             </View>
             <Text style={styles.productName}>{product.name}</Text>
-            <Text style={styles.productPrice}>Price: {product.original_price}</Text>
+            <Text style={styles.productPrice}>
+              Price: {product.original_price}
+            </Text>
             <TouchableOpacity
               style={styles.addToCartButton}
-              onPress={() => handleAddToCart(product.id)}
+              onPress={() => handleAddToCart(product)}
             >
               <Text style={styles.addToCartButtonText}>Add to Cart</Text>
             </TouchableOpacity>
@@ -123,21 +233,63 @@ function HomeScreen() {
     </ScrollView>
   );
 }
-
+////////////////////////////////////////////////////////////
 function MyCartScreen() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = (productId) => {
+    dispatch(removeFromCart(productId));
+  };
+
+  const calculateTotalPrice = () => {
+    return cart.reduce(
+      (total, product) => total + parseFloat(product.original_price),
+      0
+    );
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>My Cart Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', marginTop: 20 }}>
+        <Text style={styles.title}>My Cart</Text>
+        {cart.map((product) => (
+          <View key={product.id} style={styles.cartItemContainer}>
+            <View style={styles.cartItem}>
+              <Image
+                source={{ uri: product.thumb_img.files.webp }}
+                style={styles.cartItemImage}
+              />
+              <View style={styles.cartItemInfo}>
+                <Text style={styles.cartItemName}>{product.name}</Text>
+                <Text style={styles.cartItemPrice}>
+                  Price: {product.original_price}
+                </Text>
+              </View>
+              <TouchableOpacity
+                style={styles.removeButton}
+                onPress={() => handleRemoveFromCart(product.id)}
+              >
+                <Text style={styles.removeButtonText}>Remove</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
+        <Text style={styles.totalPriceText}>
+          Total Price: {calculateTotalPrice().toFixed(2)} 
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
+////////////////////////////////////////////
 
 function LoginScreen() {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error,setError] = useState('');
-  
+  const [error, setError] = useState('');
+
   const handleLogin = async () => {
     try {
       setError('');
@@ -157,10 +309,8 @@ function LoginScreen() {
 
       const token = response.data.data.access_token;
 
-      
       await AsyncStorage.setItem('accessToken', token);
 
-      
       navigation.navigate('Home');
     } catch (error) {
       setError('Your email or password is incorrect');
@@ -189,35 +339,71 @@ function LoginScreen() {
     </View>
   );
 }
-
+//////////////////////////////////////////////
 const Stack = createNativeStackNavigator();
 
 function MainApp() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem('accessToken');
+        if (token) {
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error('Error checking login status:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <TouchableOpacity onPress={() => navigation.navigate('MyCart')}>
-                <Text>my cart</Text>
-              </TouchableOpacity>
-            ),
-          })}
-        />
-        <Stack.Screen name="MyCart" component={MyCartScreen} />
+        {isLoggedIn ? (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={({ navigation }) => ({
+                headerRight: () => (
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('MyCart')}
+                  >
+                    <Text>My Cart</Text>
+                  </TouchableOpacity>
+                ),
+              })}
+            />
+            <Stack.Screen name="MyCart" component={MyCartScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
+///////////////////////////////////////////////
 export default function App() {
-  return <MainApp />;
+  return (
+    <Provider store={store}>
+      <MainApp />
+    </Provider>
+  );
 }
+
